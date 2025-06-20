d5e2ba3e54a31f76babf1b21a1464edc2af83277
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-06-20 22:22:21 +0700

    Update bot.py

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -7,7 +7,7 @@ import config
 class Anony(Client):
     def __init__(self):
         super().__init__(
-            name="Jonathan Robot",
+            name="Jonathan",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
